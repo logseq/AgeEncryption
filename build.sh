7fa9f22d3a04799f2e9093c596e64789dd9f3294
Andelf <andelf@gmail.com>
2022-06-07 17:47:48 +0800

    rm iossimulator build

--- build.sh
+++ build.sh
@@ -5,12 +5,25 @@ set -ex
 
 rm -rvf AgeEncryption.xcframework
 
+# iOS
 cargo build --release --target aarch64-apple-ios
+
+# simulator
+cargo build --release --target x86_64-apple-ios
+cargo build --release --target aarch64-apple-ios-sim
+
+lipo -create ./target/x86_64-apple-ios/release/libage.a \
+    ./target/aarch64-apple-ios-sim/release/libage.a \
+    -output libage_iossimulator.a
+
 
 xcodebuild -create-xcframework \
   -library ./target/aarch64-apple-ios/release/libage.a \
   -headers ./include/ \
   -output AgeEncryption.xcframework
+
+#  -library ./libage_iossimulator.a \
+#  -headers ./include/ \
 
 
 # FIXME: seems Cocoapods cannot handle this.
